8f92336e74044282e026d8f4e1cb2f04e32a8e11
Samuel Torres <rilla@MacBook-Pro.local>
2024-09-08 22:55:50 -0400

    Fetch strapi data

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,27 @@ import styles from "./page.module.scss";
 import Hero from "./components/navbar/hero/hero";
 import ContactRouterBtn from "./components/contactRouterBtn/contactRouterBtn";
 
-export default function Home() {
+async function fetchHomePageData() {
+  const currentEnvUrls: string =
+    process.env.NEXT_PUBLIC_NODE_ENV === "development"
+      ? "http://localhost:1337/api/main-pages?populate=*"
+      : "https://rillamediastrapi.onrender.com/api/main-pages";
+
+  try {
+    const res = await fetch(currentEnvUrls, { next: { revalidate: 0 } });
+    if (!res.ok) {
+      return { error: `Failed with status: ${res.status}` };
+    }
+    const data = await res.json();
+    return data;
+  } catch (error) {
+    return { error: "Error fetching data" };
+  }
+}
+
+export default async function Home() {
+  const data = await fetchHomePageData();
+  console.log("DATA: ", data);
   return (
     <main className={styles.container}>
       <Hero />
