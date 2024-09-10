8278a4abe3edd8e9f139621859e8963423abb66e
Samuel Torres <rilla@MacBook-Pro.local>
2024-09-09 22:28:00 -0400

    made mobile responsive

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,8 +2,8 @@ import Image from "next/image";
 import styles from "./page.module.scss";
 
 // components:
-import Hero from "./components/navbar/hero/hero";
-import ContactRouterBtn from "./components/contactRouterBtn/contactRouterBtn";
+import Hero from "./components/hero/hero";
+import ColoredHeadingAside from "./components/coloredHeadingAside/coloredHeadingAside";
 
 async function fetchHomePageData() {
   const currentEnvUrls: string =
@@ -29,33 +29,7 @@ export default async function Home() {
   return (
     <main className={styles.container}>
       <Hero />
-      <div className={styles.landingAboutUs}>
-        <div className={styles.textContainer}>
-          <div className={styles.leftSide}>
-            <h2>
-              <span className={styles.highlight}>Empowering</span> brands,
-            </h2>
-            <h2>
-              While <span className={styles.highlight}>Fueling Growth!</span>
-            </h2>
-          </div>
-          <div className={styles.rightSide}>
-            <h3>Who We Are</h3>
-            <p>
-              We are a dynamic web design agency—a team of visionaries and
-              disruptors poised to tackle your brand’s toughest challenges. Our
-              mission is to break away from the ordinary and collaborate with
-              you to reach ambitious goals. We excel in helping brands assert
-              their unique presence and drive continuous growth. Ready to
-              elevate your digital footprint? Let’s make it happen.
-            </p>
-            <ContactRouterBtn borderColor="black" initialFontColor="black" />
-          </div>
-        </div>
-        <div className={styles.borderBottom}>
-          <p></p>
-        </div>
-      </div>
+      <ColoredHeadingAside />
     </main>
   );
 }
